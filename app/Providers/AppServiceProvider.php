a46095eb2637bec214592ea527fd14c2e7e4ae20
Tayfun ÇELİK <tayfun@neturk.com>
2020-05-24 19:21:15 +0300

    Provider error that occurred during migrate operation was resolved.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share('config', Config::find(1));
+
     }
 }
